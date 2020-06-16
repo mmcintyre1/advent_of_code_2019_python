3d5718d97a9018da9bd0c04960e6cd7b9c1b2e28
Michael <mmcintyre@ebsco.com>
2020-06-15 21:40:33 -0400

    day 11 part 1

--- 11/part_1.py
+++ 11/part_1.py
@@ -1,30 +1,51 @@
+import typing
+
 from Computer import IntCodeComputer
 from puzzle_input import puzzle_input
 
-computer = IntCodeComputer(puzzle_input)
 
-current_position = (0, 0)
-visited_positions = {}
+class HullPainting:
 
-instruction = 0
-directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-current_direction = 0
+    visited_positions: typing.Dict[typing.Tuple[int, int], int] = {}
+    directions: typing.List[typing.Tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-while not computer.done:
-    color = computer.compute(custom_input=instruction, return_on_output=True)
-    direction = computer.compute(return_on_output=True)
+    def __init__(self, computer: IntCodeComputer):
+        self.computer = computer
+        self.current_instruction: int = 0
+        self.current_direction: int = 0
+        self.current_position: tuple = (0, 0)
 
-    if direction == 0:
-        adder = -1
-    else:
-        adder = 1
+    def change_direction(self, new_direction):
+        """Changes current direction, using modulo to make sure things stay within the directional bounds."""
+        if new_direction == 0:
+            adder = -1
+        else:
+            adder = 1
 
-    current_direction = (current_direction + adder) % 4
-    current_position = tuple(sum(x) for x in zip(current_position, directions[current_direction]))
-    visited_positions[current_position] = color
+        self.current_direction = (self.current_direction + adder) % 4
 
-    instruction = color
+    def walk(self):
+        """Increments current position by one based on current direction."""
+        self.current_position = tuple(
+            sum(x) for x in zip(self.current_position, self.directions[self.current_direction])
+        )
 
-print(visited_positions)
+    def paint(self):
+        while not self.computer.done:
+            color = self.visited_positions[self.current_position] if self.current_position in self.visited_positions else 0
+            self.visited_positions[self.current_position] = self.computer.compute(custom_input=color, return_on_output=True)
+            self.change_direction(self.computer.compute(return_on_output=True))
+            self.walk()
+
+
+def main():
+    computer = IntCodeComputer(puzzle_input)
+    painting = HullPainting(computer)
+    painting.paint()
+    print(len(painting.visited_positions))
+
+
+if __name__ == '__main__':
+    main()
 
 
