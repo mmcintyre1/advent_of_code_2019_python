b0780fbf8c17ccf0efd1f40111550fe30299560e
Michael <mmcintyre@ebsco.com>
2020-06-18 22:30:58 -0400

    day 13 part 1

--- 09/part_1_part_2.py
+++ 09/part_1_part_2.py
@@ -4,7 +4,7 @@ from puzzle_input import puzzle_input
 
 if __name__ == '__main__':
     computer = Computer(puzzle_input)
-    computer.compute(1)
+    computer.compute(1, silence=False)
 
     computer.reset()
-    computer.compute(2)
+    computer.compute(2, silence=False)
